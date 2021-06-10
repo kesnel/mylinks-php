c5273044b4ae10ba222dac03416a0ec9d24343c4
Kesnel <kesnelsamuel@gmail.com>
2021-06-09 21:01:49 -0400

    New repository

---
+++ index.php
@@ -0,0 +1,165 @@
+<!--
+ █████   ████                                      ████      █████████                                                ████ 
+░░███   ███░                                      ░░███     ███░░░░░███                                              ░░███ 
+ ░███  ███     ██████   █████  ████████    ██████  ░███    ░███    ░░░   ██████   █████████████   █████ ████  ██████  ░███ 
+ ░███████     ███░░███ ███░░  ░░███░░███  ███░░███ ░███    ░░█████████  ░░░░░███ ░░███░░███░░███ ░░███ ░███  ███░░███ ░███ 
+ ░███░░███   ░███████ ░░█████  ░███ ░███ ░███████  ░███     ░░░░░░░░███  ███████  ░███ ░███ ░███  ░███ ░███ ░███████  ░███ 
+ ░███ ░░███  ░███░░░   ░░░░███ ░███ ░███ ░███░░░   ░███     ███    ░███ ███░░███  ░███ ░███ ░███  ░███ ░███ ░███░░░   ░███ 
+ █████ ░░████░░██████  ██████  ████ █████░░██████  █████   ░░█████████ ░░████████ █████░███ █████ ░░████████░░██████  █████
+░░░░░   ░░░░  ░░░░░░  ░░░░░░  ░░░░ ░░░░░  ░░░░░░  ░░░░░     ░░░░░░░░░   ░░░░░░░░ ░░░░░ ░░░ ░░░░░   ░░░░░░░░  ░░░░░░  ░░░░░ 
+-->
+
+<!DOCTYPE html>
+<html lang="en" class="html-bg">
+<head>
+
+<meta charset="UTF-8">
+<meta name="viewport"   content="width=device-width, initial-scale=1.0">
+
+<title> Kesnel Samuel </title>
+
+<meta name="theme-color"        content="#09B1BA">
+<meta name="language"           content="English">
+<meta name="Copyright"          content="Kesnel, LLC">
+<meta name="author"             content="Kesnel Samuel">
+<meta name="application-name"   content="kesnel">
+
+<meta name="ROBOTS"             content="ALL">
+<meta name="google"             content="notranslate">
+<meta name="robots"             content="index, follow">
+<meta name="googlebot"          content="index, follow">
+
+<meta name="keywords"           content="kesnel samuel jean philippe, data scientist, web developer, haitian in tech, tech, stem">
+<meta name="description"        content="">
+
+<!-- Open Graph / Facebook -->
+<meta property="og:site_name"          content="tutipad">
+<meta property="og:type"               content="website">
+<meta property="og:url"                content="https://kesnel.link/">
+<meta property="og:title"              content="Kesnel Samuel">
+<meta property="og:description"        content="">
+<meta property="og:image"              content="static/img/card/kesnel-samuel-cover.jpeg">
+<meta property="og:image:secure_url"   content="static/img/card/kesnel-samuel-cover.jpeg">
+
+<!-- Twitter Card -->
+<meta name="twitter:card"              content="summary_large_image">
+<meta name="twitter:type"          	   content="website">
+<meta name="twitter:url"               content="https://kesnel.link/">
+<meta name="twitter:site"          	   content="@kesnelsamuel">
+<meta name="twitter:creator"           content="@kesnelsamuel">
+<meta name="twitter:title"             content="Kesnel Samuel">
+<meta name="twitter:description"       content="">
+<meta name="twitter:image"             content="static/img/card/kesnel-samuel-cover.jpeg">
+
+<!-- Fav Icons -->
+<link rel="apple-touch-icon" sizes="57x57"      href="static/img/fav-ico/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60"      href="static/img/fav-ico/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72"      href="static/img/fav-ico/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76"      href="static/img/fav-ico/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114"    href="static/img/fav-ico/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120"    href="static/img/fav-ico/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144"    href="static/img/fav-ico/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152"    href="static/img/fav-ico/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180"    href="static/img/fav-ico/apple-icon-180x180.png">
+
+<link rel="icon" type="image/png" sizes="192x192"   href="static/img/fav-ico/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32"     href="static/img/fav-ico/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96"     href="static/img/fav-ico/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16"     href="static/img/fav-ico/favicon-16x16.png">
+<link rel="manifest" href="static/img/fav-ico/manifest.json">
+
+<!-- Additional Meta Tags -->
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="static/img/fav-ico/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+
+<!-- %%%%%%%%  -->
+<link rel="stylesheet" href="static/asset/css/base.css">
+
+<!-- % Global site tag (gtag.js) - Google Analytics   % -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-75L6Z49PCR"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-75L6Z49PCR');
+</script>
+
+
+</head>
+<body>
+
+    <!-- %%% -->
+     
+    <main id="main-app" class="main-app page-top">
+
+        <section class="LnK" id="LnK">
+            <div class="bg">
+                <div class="container">
+                    <div class="ti_section">
+
+                        <!-- Profil -->
+                        <figure class="profil">
+                            <img src="static/img/profil/kesnel-samuel-jean-philippe.jpeg" alt="Profil picture of Kesnel Samuel Jean Philippe" class="avatar">
+                        </figure>
+            
+                        <!-- Introduction -->
+                        <div class="cardUp">
+                            <h2 class="name"> Kesnel Samuel Jean-Philippe </h2>
+                            <h4 class="pseudo"> @kesnelsamuel  </h4>
+                        </div>
+            
+                        <!-- Links -->
+                        <div class="cardDown">
+                            <div class="cardDownIN">
+                                <ul class="CardUl">
+                                    <li class="CardUlLi">
+                                        <a href="https://www.kesnel.me/" target="_blank" rel="noopener noreferrer" class="CardLiA"> 
+                                        <span class="text"> Personal Website </span>
+                                        </a>
+                                    </li>
+                                    <li class="CardUlLi">
+                                        <a href="https://www.instagram.com/kesnelsamuel/" target="_blank" rel="noopener noreferrer" class="CardLiA"> 
+                                        <span class="text"> Follow me on Instagram </span>
+                                        </a>
+                                    </li>
+                                    <li class="CardUlLi">
+                                        <a href="https://www.linkedin.com/in/kesnel-s-jean-philippe-7ba515169/" target="_blank" rel="noopener noreferrer" class="CardLiA"> 
+                                        <span class="text"> Connect with me on LinkedIn </span>
+                                        </a>
+                                    </li>
+                                    <li class="CardUlLi">
+                                        <a href="https://facebook.com/kesnelsjp" target="_blank" rel="noopener noreferrer" class="CardLiA"> 
+                                        <span class="text"> Add me on Facebook </span>
+                                        </a>
+                                    </li>
+                                    <li class="CardUlLi">
+                                        <a href="https://twitter.com/kesnelsamuel" target="_blank" rel="noopener noreferrer" class="CardLiA"> 
+                                        <span class="text"> Follow me on Twitter </span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+            
+                        <!-- Footer -->
+                        <div class="cardFooter">
+                            <p class="cc"> © 2021 <span class="break"> Kesnel Samuel. </span> All right reserved </p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </main>
+
+    <!-- %%% -->
+
+    
+<script src="static/asset/js/jquery.js"></script>
+<script src="static/asset/js/main.js"></script>
+
+</body>
+</html>
